1c00649a4bd9b83e393c200c70785ae78097a60f
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-02 12:33:01 +0530

    Updated code

--- area_of_triangle.java
+++ area_of_triangle.java
@@ -5,9 +5,9 @@ class Main
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the first side of the rectangle");
+		System.out.println("Enter the first side of the rectangle");
 		int a = scan.nextInt();
-		System.out.print("Enter the second side of the rectangle");
+		System.out.println("Enter the second side of the rectangle");
 		int b = scan.nextInt();
 		int area=a*b;
 		System.out.println("Area of the rectangle is "+area);
